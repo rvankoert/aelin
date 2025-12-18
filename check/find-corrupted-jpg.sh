248031b49c1a0b2de2d85bb9610474f8eadce43a
Ssssssuuunny <144811989+Ssssssuuunny@users.noreply.github.com>
2025-12-18 16:15:39 +0100

    Stefan's solution for the race condition

--- check/find-corrupted-jpg.sh
+++ check/find-corrupted-jpg.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-numcores=$(nproc --all)
-numcores=1
-if [ -z "$1" ]; then echo "please provide path to images to be checked" && exit 1; fi;
+input_dir="$1"
+if [ -z "$input_dir" ] ; then
+    echo "please provide path to images to be checked"
+    exit 1
+fi
 
-find -L $1 -name '*.jpg' -type f| xargs -n 1 -P $numcores -I % sh -c 'identify -regard-warnings -verbose % > /dev/null 2>&1;echo % $?'
+# default to 1 thread if not provided
+numthreads="${2:-1}"
+
+output_file="${input_dir}/corrupted_jpgs_summary.txt"
+
+# check jpg images for corruption
+find -L "$input_dir" -name '*.jpg' -type f |
+    xargs -P $numthreads -I % sh -c '
+        (identify -regard-warnings -verbose "%" >/dev/null 2>&1 && echo "% 0") || echo "% 1"
+    ' | tee "$output_file"
